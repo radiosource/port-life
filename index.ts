0dd563e0812d7c10da62763cba33ebe932aaeecc
zagnit <oleh.zahnitko@ria.com>
2019-10-08 21:17:39 +0300

    [feathure] - [bagfix] - Fixed case with return bool value in messenger [other] -

--- index.ts
+++ index.ts
@@ -1,9 +1,5 @@
 import {App} from "./app/App";
-import {App1} from "./app/App1";
 
 document.addEventListener('DOMContentLoaded', () => new App());
-// document.addEventListener('DOMContentLoaded', () => {
-//     console.log(new App1())
-// });
 
 
